8d369205caa7347eb44d121ba97c7aafcb242756
meeky92 <112586695+meeky92@users.noreply.github.com>
2022-11-02 09:09:37 -0500

    first commit

---
+++ homework.js
@@ -0,0 +1,76 @@
+/*
+    Exercise 1: Write a function called toCelcius that takes in a number representing 
+    temperature in Fahrenheit and converts the number into Celcius.
+
+    Ex 1. toCelcius(32) // 0
+    Ex 2. toCelcius(212) // 100
+    Ex 3. toCelcius(50) // 10
+*/
+
+function toCelsius(num){
+    return (num - 32)*(5/9);
+}
+// let toCelsius2 = num => ((num - 32)*(5/9))
+// console.log(toCelsius2(212))
+console.log(toCelsius(32))
+console.log(toCelsius(212))
+console.log(toCelsius(50))
+
+
+/*
+    Exercise 2: Create a class for a User that sets username and password property
+    on creation. Add a method to change the user's password. The method should take in 
+    the old password and new password. If the old password is wrong do not reset the password.
+    Create an instance of the new user and then change their password using the method.
+*/
+
+// still going over lecture notes on this =(
+
+// class User{
+//     constructor(username, password){
+//         this.username = username;
+//         this.password = password;
+//         this.newpass = newpass
+//     }
+//     login(){
+//         console.log(this.username, this.password);
+//     }
+//     changepass(){
+//         console.log(this.password, this.newpass)
+//     }
+// }
+// var userOne = new User('emk', '123')
+
+
+/*
+    Exercise 3: Write a function that takes in an array of integers and returns
+    an array of the negative integers squared using Array methods
+
+    Ex 1. squareNegatives([-10, -3, 4, -2, 8, 9, -3]) // [100, 9, 4, 9]
+    Ex 2. squareNegatives([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]) // [25, 16, 9, 4, 1]
+    Ex 3. squareNegatives([2, 4, 6, 8, 10]) // []
+*/
+console.log("=====")
+// function squareNegatives(arr){
+//     // let newArr = [];
+//     for (let n of arr){
+//         if (n < 0){
+//             console.log(n*n)
+//         };
+//     };
+// };
+
+// let squareNegatives = arr => arr.filter(num => num < 0)
+
+function squareNegatives(arr) {
+    newArr = [];
+    for (let x = 0; x < arr.length; x++) {
+        if (arr[x] < 0) newArr.push(arr[x] * arr[x]);
+    };
+    return newArr
+};
+console.log(squareNegatives([-10, -3, 4, -2, 8, 9, -3]))
+
+
+
+
